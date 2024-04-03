a8bac354ad6a7f82a1f31775625c742648c81abe
Cody Spratford <koreanpanda345@gmail.com>
2024-04-03 12:48:06 -0700

    testing

--- data/mods/gen9pokemonnext/formats-data.ts
+++ data/mods/gen9pokemonnext/formats-data.ts
@@ -1,4 +1,12 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	syclar: {
+		inherit: true,
+		tier: "LC",
+	},
+	syclant: {
+		inherit: true,
+		tier: "OU",
+	},
 	venusaur: {
 		inherit: true,
 		isNonstandard: null,
