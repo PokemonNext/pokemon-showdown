5719070a5ae711a81350c16f89a2bae72c0828a3
Cody Spratford <koreanpanda345@gmail.com>
2024-04-03 13:38:12 -0700

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -22,7 +22,7 @@ export const Formats: FormatList = [
 		section: "Pokemon Next: Regional Dex",
 	},
 	{
-		name: "[Gen 9] Pokemon Next National Dex",
+		name: "[Gen 9] Pokemon Next OU",
 		desc: "Pokemon Next Official Format",
 		mod: 'gen9pokemonnext',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
