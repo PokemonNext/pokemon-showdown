1866da1130a7956ae44e0dd6261a9759469156c1
skipprthepenguin <sagie2006p@gmail.com>
2024-05-23 01:37:57 +0300

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -2295,14 +2295,4 @@ export const Formats: FormatList = [
 			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
 		],
 	},
-	{
-		name: "Pokemon Custom",
-		mod: 'gen9pokemonnext',
-		searchShow: false,
-		debug: true,
-		battle: {trunc: Math.trunc},
-		// no restrictions, for serious (other than team preview)
-		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
-	},
-
 ];
